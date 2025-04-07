a3b7924d14b3585b3effeb617dc51b26d15d33db
Cooper L <55725860+XCoop@users.noreply.github.com>
2025-04-07 22:53:13 +0000

    Commit

--- challenge1.py
+++ challenge1.py
@@ -1,7 +1,7 @@
 l_1 = [1,3,2,4,5]
 l_2 = ["a","b","c","d","e"]
 l_1.sort()
-'''
+
 def print_table():
     global l_1
     global l_2
@@ -23,8 +23,8 @@ l_1.sort()
 print_table()
 
 
-'''
-s_1 = ','.join(str(x) for x in l_1)
+
+s_1 = ",".join(str(x) for x in l_1)
 s_2 = ",".join(str(x) for x in l_2)
 
 file = open("challenge1.txt", "w")
